9aab3c03913bdf2eccfddf78db616bfb0762c2cd
Coulibaly Nonta <coulibalynonta@gmail.com>
2017-12-09 21:50:59 +0000

    Build with Linaro 6.3

--- build.sh
+++ build.sh
@@ -8,11 +8,9 @@ mkdir -p out
 export USE_CCACHE=1
 export ARCH=arm64 ARCH_MTK_PLATFORM=mt6755
 export SUBARCH=arm64
-export CROSS_COMPILE=/home/nonta72/linaro49/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=/home/nonta72/linaro63/bin/aarch64-linux-gnu-
 
-#Defconfig for K6000Plus(and Y6)
-make -C $PWD O=$PWD/out ARCH=arm64 hct6750_66_n_defconfig
-#make ARCH=arm64 k6000_plus_defconfig
+#Defconfig for Lone_Kernel
+make -C $PWD O=$PWD/out ARCH=arm64 lone_defconfig
 
 make -j4 -C $PWD O=$PWD/out ARCH=arm64
-#make -j4 ARCH=arm64
